6dc161517679e3829188a39ec5c70e09a2a0eda4
nirmalearn <nirmalmangalat.learning@gmail.com>
2025-04-23 14:33:32 +0530

    auth lunk for bk

--- src/services/tokenService.ts
+++ src/services/tokenService.ts
@@ -1,6 +1,6 @@
 // src/services/tokenService.ts
 export const getBackendToken = async (): Promise<string> => {
-    const res = await fetch('/api/get-token'); // this will go to your Express route
+    const res = await fetch('https://api.nameurcloud.com/api/get-token'); // this will go to your Express route
     if (!res.ok) throw new Error('Token fetch failed');
     const data = await res.json();
     return data.token;
