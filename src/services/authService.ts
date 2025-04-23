83bc7e0f0c70e3d85ad025895ec9b1e36069e369
nirmalearn <nirmalmangalat.learning@gmail.com>
2025-04-23 12:32:57 +0530

    http tp https

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,7 +2,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 export async function getIdToken(targetAudience: string): Promise<string> {
   const res = await fetch(
-    `http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=${targetAudience}`,
+    `https://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=${targetAudience}`,
     {
       headers: {
         "Metadata-Flavor": "Google",
