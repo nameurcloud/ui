cc83b063275925d5e22ba5e63c455d7c35dd1e3e
nirmalearn <nirmalmangalat.learning@gmail.com>
2025-04-27 18:21:02 +0530

    try

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -12,7 +12,7 @@ export const registerUser = async (email: string, password: string,fname: string
 };
 
 export const loginUser = async (email: string, password: string) => {
-  const res = await fetch(`${API_URL}/login`, {
+  const res = await fetch(`api/login`, {
     method: 'POST',
     headers: { 
       'Content-Type': 'application/json',
