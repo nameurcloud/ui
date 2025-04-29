94ddd941c17ca0d8599b0bf69e7114f5961dd8bb
nirmalearn <nirmalmangalat.learning@gmail.com>
2025-04-29 15:47:24 +0530

    removed import validate session from dashboard

--- src/pages/insider/dashboard.tsx
+++ src/pages/insider/dashboard.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getToken, validateSession } from "../../services/authService";
+import { getToken } from "../../services/authService";
 
 const Dashboard = () => {
   const [user, setUser] = useState<any>(null);
