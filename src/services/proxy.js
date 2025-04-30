7e03437826865659b4da2a9cb4bd2c4ef05e35f3
nirmalearn <nirmalmangalat.learning@gmail.com>
2025-05-01 01:50:27 +0530

    try

--- src/services/proxy.js
+++ src/services/proxy.js
@@ -105,8 +105,11 @@ app.use((req, res, next) => {
 });
 
 app.get('*', (req, res) => {
-  const filePath = path.resolve('dist', 'index.html');
-  console.log('Serving file from:', filePath);  // Log the absolute path to index.html
+ // const filePath = path.resolve(__dirname, 'dist', 'index.html');
+ // console.log('Serving file from:', filePath);  // Log the absolute path to index.html
+ // res.sendFile(filePath);
+ const filePath = '/app/dist/index.html';
+  console.log('Serving file from (hard-coded):', filePath);
   res.sendFile(filePath);
 });
 
